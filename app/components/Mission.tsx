105576128e6b7bd13ebbdd1191a97ea32e4daa55
Haytham Hlioui <hamguin@Haythams-MacBook-Air.local>
2026-03-24 19:30:54 -0400

    Added ability to mark missions as done. Still not fully functioning since it doesn't effect the array or add a done tag (remove any in progress or new tags)

--- app/components/Mission.tsx
+++ app/components/Mission.tsx
@@ -9,9 +9,12 @@ interface MissionProps {
 
 export default function Mission({ mission }: MissionProps) {
   const [showTags, setShowTags] = useState(false);
+  const [done, setDone] = useState(false);
+
+  //later will add a fullcontext function that removed new tag and add Done tag
 
   return (
-    <div className="flex justify-center items-center w-full">
+    <div className={`flex justify-center items-center w-full ${done ? "hidden" : ""}`}>
       <div className="flex flex-col w-full px-5 py-4 rounded-lg bg-gray-300 hover:bg-gray-300 shadow-sm">
 
         {/* Top row: title + toggle button */}
@@ -19,28 +22,52 @@ export default function Mission({ mission }: MissionProps) {
           <p className="text-sm font-medium text-gray-900">
             {mission.title}
           </p>
-          <button
-            type="button"
-            onClick={() => setShowTags(prev => !prev)}
-            className="text-gray-500 hover:text-gray-400"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-circle-alert-icon lucide-circle-alert"
+
+          <div className="flex space-x-2.5 -mr-2">
+            <button
+              type="button"
+              onClick={() => setShowTags(prev => !prev)}
+              className="text-gray-500 hover:text-gray-400"
             >
-              <circle cx="12" cy="12" r="10" />
-              <line x1="12" y1="8" x2="12" y2="12" />
-              <line x1="12" y1="16" x2="12.01" y2="16" />
-            </svg>
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-circle-alert-icon lucide-circle-alert"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <line x1="12" y1="8" x2="12" y2="12" />
+                <line x1="12" y1="16" x2="12.01" y2="16" />
+              </svg>
+            </button>
+
+            <button
+              type="button"
+              onClick={() => setDone(prev => !prev)}
+              className="text-gray-500 hover:text-gray-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-circle-check-icon"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <path d="M9 12l2 2 4-4" />
+              </svg>
+            </button>
+          </div>
         </div>
 
         {/* Tags row */}
