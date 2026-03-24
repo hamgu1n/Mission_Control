a3b0719a5909905b71be87faabe1cd58bbb926f5
Haytham Hlioui <hamguin@Haythams-MacBook-Air.local>
2026-03-24 19:06:26 -0400

    Added icon for missions tags and

--- app/components/Mission.tsx
+++ app/components/Mission.tsx
@@ -12,7 +12,7 @@ export default function Mission({ mission }: MissionProps) {
 
   return (
     <div className="flex justify-center items-center w-full">
-      <div className="flex flex-col w-full px-5 py-4 rounded-lg bg-gray-300 shadow-sm">
+      <div className="flex flex-col w-full px-5 py-4 rounded-lg bg-gray-300 hover:bg-gray-300 shadow-sm">
 
         {/* Top row: title + toggle button */}
         <div className="flex justify-between items-center">
@@ -24,7 +24,22 @@ export default function Mission({ mission }: MissionProps) {
             onClick={() => setShowTags(prev => !prev)}
             className="text-gray-500 hover:text-gray-400"
           >
-            @
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-circle-alert-icon lucide-circle-alert"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <line x1="12" y1="8" x2="12" y2="12" />
+              <line x1="12" y1="16" x2="12.01" y2="16" />
+            </svg>
           </button>
         </div>
 
